9434b16060bbb12fa8b1b69949501550e2b18485
Marcos Mayorga <marxoh22@gmail.com>
2023-05-08 20:08:54 -0400

    backendOK

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ import express from 'express';
 //conectarse con cors
 import cors from 'cors';
 import morgan from'morgan';
+
+import swaggerJSdoc from'swagger-jsdoc';
+import swaggerUI from'swagger-ui-express';
+import {options} from './swaggerOptions';
+
+const swaggerSpecs = swaggerJSdoc(options);
+
 import tasksRoutes from './routes/tasks';
 
 const app = express();
@@ -16,5 +23,9 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(tasksRoutes)
+
+//cuando se accesa a /docs an la api
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 
 export default app;
--- src/config.js
+++ src/config.js
@@ -2,8 +2,6 @@
 import {config as dotenv} from 'dotenv';
 dotenv();
 
-console.log(process.env.NICK)
-
 export const config = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
