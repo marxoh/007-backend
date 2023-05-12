faeb8f0c992b54edbfb5da34d99d6f9ba42fae25
Marcos Mayorga <marxoh22@gmail.com>
2023-05-11 21:59:20 -0400

    postventa

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -10,65 +10,65 @@ import {
 
 const router = Router();
 
-/**
- * @swagger
- * tags:
- *  name: Tasks
- *  description: tasks endpoint
- */
+// /**
+//  * @swagger
+//  * tags:
+//  *  name: Tasks
+//  *  description: tasks endpoint
+//  */
 
-/**
- * @swagger
- * /tasks:
- *  get:
- *    summary: obtiene todas las tareas
- *    tags: [Tasks] //pertenece al tag Tasks
- */
+// /**
+//  * @swagger
+//  * /tasks:
+//  *  get:
+//  *    summary: obtiene todas las tareas
+//  *    tags: [Tasks] //pertenece al tag Tasks
+//  */
 router.get("/tasks", getTasks);
 
-/**
- * @swagger
- * /tasks/count:
- *  get:
- *    summary: obtiene el total de las tareas
- *    tags: [Tasks]
- */
+// /**
+//  * @swagger
+//  * /tasks/count:
+//  *  get:
+//  *    summary: obtiene el total de las tareas
+//  *    tags: [Tasks]
+//  */
 router.get("/tasks/count", getTaskCount);
 
-/**
- * @swagger
- * /tasks/{id}:
- *  get:
- *    summary: Get task by Id
- *    tags: [Tasks]
- */
+// /**
+//  * @swagger
+//  * /tasks/{id}:
+//  *  get:
+//  *    summary: Get task by Id
+//  *    tags: [Tasks]
+//  */
 router.get("/tasks/:id", getTask);
 
-/**
- * @swagger
- * /tasks:
- *  post:
- *    summary: crear una tarea
- *    tags: [Tasks]
- */
+// /**
+//  * @swagger
+//  * /tasks:
+//  *  post:
+//  *    summary: crear una tarea
+//  *    tags: [Tasks]
+//  */
 router.post("/tasks", saveTasks);
 
-/**
- * @swagger
- * /tasks:
- *  delete:
- *    summary: borrar una tarea
- *    tags: [Tasks]
- */
+// /**
+//  * @swagger
+//  * /tasks:
+//  *  delete:
+//  *    summary: borrar una tarea
+//  *    tags: [Tasks]
+//  */
 router.delete("/tasks/:id", deleteTasks);
 
-/**
- * @swagger
- * /tasks:
- *  put:
- *    summary: actualizar una tarea
- *    tags: [Tasks]
- */
+// /**
+//  * @swagger
+//  * /tasks:
+//  *  put:
+//  *    summary: actualizar una tarea
+//  *    tags: [Tasks]
+//  */
 router.put("/tasks/:id", updateTasks);
 
 export default router;
